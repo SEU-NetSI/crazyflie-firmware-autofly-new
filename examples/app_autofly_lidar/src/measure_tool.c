adfac41ed4e6d830d707c6043d6ccfd6bb73294d
shang-huan <2629045949@qq.com>
2023-05-20 16:12:26 +0800

    add the board limitation

--- examples/app_autofly_lidar/src/measure_tool.c
+++ examples/app_autofly_lidar/src/measure_tool.c
@@ -39,7 +39,12 @@ void get_Current_point(coordinateF_t* cureent_point){
 }
 
 bool ReliabilityTest(coordinateF_t* last, coordinateF_t* cur){
-    return abs(last->x-cur->x)+abs(last->y-cur->y)+abs(last->z-cur->z) < RELIABILITY_DISTANCE;
+    bool res = true;
+    res &= abs(last->x-cur->x)+abs(last->y-cur->y)+abs(last->z-cur->z) < RELIABILITY_DISTANCE;
+    res &= abs(cur->x) <= WIDTH_X;
+    res &= abs(cur->y) <= WIDTH_Y;
+    res &= abs(cur->z) <= WIDTH_Z;
+    return res;
 }
 
 
